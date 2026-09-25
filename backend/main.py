16dd6e8fdf682f49c516fa4efa60e1c0fd7371ee
Sonu Jaiswal <jsonu77768@gmail.com>
2026-09-25 13:58:12 +0530

    Update CORS settings to allow multiple origins

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""https://photo-sharing-access-system.vercel.app""],
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:5173",
+        "https://photo-sharing-access-system.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(admin_router, prefix="/admin", tags=["Admin"])
 app.include_router(group_router, prefix="/group", tags=["Group"])
