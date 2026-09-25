c3062a4f33c55380d5a6f95b088a146727afea71
Sonu Jaiswal <jsonu77768@gmail.com>
2026-09-25 12:59:59 +0530

    Use timezone-aware datetime for expiration time
    Updated expiration time calculation to use timezone-aware datetime.

--- backend/access_requests/controller.py
+++ backend/access_requests/controller.py
@@ -7,7 +7,7 @@ from models.session import Session as UserSession
 from utils.security import verify_password
 from utils.code_generator import generate_random_code
 from sessions.controller import generate_unique_session_token
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 def generate_unique_request_code(db: Session) -> str:
     while True:
@@ -26,7 +26,7 @@ def create_access_request(request_data: AccessRequestCreate, db: Session):
         raise HTTPException(status_code=404, detail="Invalid Group code or Password")
 
     request_code = generate_unique_request_code(db)
-    expires_at = datetime.utcnow() + timedelta(minutes=60)
+    expires_at = datetime.now(timezone.utc) + timedelta(minutes=60)
 
     request_object = AccessRequest(group_id=existing.id, request_code=request_code, expires_at=expires_at)
     db.add(request_object)
